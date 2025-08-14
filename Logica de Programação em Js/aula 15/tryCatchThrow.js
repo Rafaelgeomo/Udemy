b06635d3c7df56e55ff8072bccc9230172867a81
Rafael Jeronymo <jeronymo.rafael@gmail.com>
2025-08-14 14:10:18 -0300

    continuação da aula de tratamento de erro

--- Logica de Programação em Js/aula 15/tryCatchThrow.js
+++ Logica de Programação em Js/aula 15/tryCatchThrow.js
@@ -1,15 +1,62 @@
-function soma(x, y) {
-   if (typeof x !== 'number' || typeof y !== 'number') {
-      throw new Error('x e y precisam ser números.');
+function retornaHora(data) {
+   if (data && !(data instanceof Date)) {
+      throw new TypeError('Esperando instância de Date.');
    }
 
-   return x + y;
+   if (!data) {
+      data = new Date();
+   }
+
+   return data.toLocaleTimeString('pt-BR', {
+
+   });
 }
 
 try {
-   console.log(soma(1, 2));
-   console.log(soma('1', 2));
-} catch (error) {
-   // console.log(error);
-   console.log('Alguma coisa mais amigavel pro usuáro')
+   const data = new Date('01-01-1970 12:58:12')
+   const hora = retornaHora();
+   console.log(hora);
+} catch (e) {
+
+} finally {
+   console.log('Tenha um bom dia.')
 }
+
+
+
+
+// try {
+//    // É executada quando não há erros
+//    console.log('Abri um arquivo');
+//    console.log('Manipulei o arquivo e gerou erro');
+//    console.log(a);
+//    console.log('Fechei o arquivo');
+
+// } catch (e) {
+//    // É executada quando há erros
+//    console.log('Tratando o erro');
+// } finally {
+//    // Sempre será
+//    console.log('FINALLY: Eu sempre sou executado');
+// }
+
+
+
+
+
+
+// function soma(x, y) {
+//    if (typeof x !== 'number' || typeof y !== 'number') {
+//       throw new Error('x e y precisam ser números.');
+//    }
+
+//    return x + y;
+// }
+
+// try {
+//    console.log(soma(1, 2));
+//    console.log(soma('1', 2));
+// } catch (error) {
+//    // console.log(error);
+//    console.log('Alguma coisa mais amigavel pro usuáro')
+// }
